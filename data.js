7bf4cba95ab633ab12cc2ec880d6d847d4c4e5ee
caravena180 <c.aravena180@gmail.com>
2026-06-15 20:30:47 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+let productos = [
+    { id: 1,
+    nombre: "Filtro de aceite Corsa", precio: 8500, categoria: "Repuestos" },
+    
+    { id: 2, nombre: "Radio con pantalla táctil", precio: 45000, categoria: "Accesorios" }
+];
+
+module.exports = productos;
